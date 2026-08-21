11399f3b8ddd9169e1defc47c41d21d186dbdcbe
makium <marco.cassarino@gmail.com>
2026-08-21 22:41:17 +0200

    Initial Commit

---
+++ Helpme/HelpmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpmeApp.swift
+//  Helpme
+//
+//  Created by Marco on 21/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct HelpmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
